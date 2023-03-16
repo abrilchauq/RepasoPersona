c241e51268ad9ddb2b6b6348579c8d9a2ee9bb8e
abrilchauq <abril.chauqueet12d1@gmail.com>
2023-03-16 15:51:30 -0300

    update movimiento

--- RepasoPersona.Core/Movimiento.cs
+++ RepasoPersona.Core/Movimiento.cs
@@ -2,6 +2,15 @@ namespace RepasoPersona.Core
 {
     public abstract class Movimiento
     {
-        
+        public double monto { get; protected set; }
+    }
+
+    public double Debitar
+    {
+        get
+        {
+
+        }
+
     }
 }
